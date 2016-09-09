69610a248354c9941e5094027e22e9aec15051cf
Jeffry Romero <jeffryromero@unitec.edu>
2016-09-09 12:32:23 -0600

    Ordenado de la informacion

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,11 +1,8 @@
 /**
  * Created by Jeffry Romero on 08/09/2016.
  */
-angular.module('ParkingApp').controller('MainController', function ($scope,$firebaseObject,$firebaseArray,FBURL) {
+pcApp.controller('MainController', function ($scope,$firebaseArray,FBURL) {
     var test=new Firebase(FBURL.url+"floors");
-    //test.orderByChild('id');
-    //var test=new Firebase('https://jeffry-firebaseapp.firebaseio.com/jobs');
-    $scope.floors=$firebaseObject(test);
+    $scope.floors=$firebaseArray(test);
     console.log($scope.floors);
-    //jobs.$bindTo($scope,"data");
 });
